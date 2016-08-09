550f9838e65378b74c0d5a04a5999a9d932a89d0
Yamkovoi.a <fagoth0509@gmail.com>
2016-08-09 14:56:33 +0300

    Create Slider need check .active

--- _js/slider.js
+++ _js/slider.js
@@ -1,4 +1,4 @@
-"to String"
+
  /*function add(x){
 	x.style.height = "75px";
 	x.style.width = "75px";
@@ -15,31 +15,113 @@ var rem = function(x){
 
 
 
-window.onload = function(){
+	/*
+  var elements = $(".img_pos");
+  console.log(elements)
+
+     next = function(){
+      var active = elements.filter('.active').removeClass('active');
+        var nextElement = active.next().length?active.next():elements.eq(0);
+        nextElement.addClass("active");
+
+        console.log(active)
+
+
+    };
+         prev = function(){
+      //i_act = $('.active').removeClass('active').prev().addClass('active');
+      //  console.log(i_act);
+
+          var active = elements.filter('.active').removeClass('active');
+            var prevElement = active.prev().length?active.prev():elements.eq(-1);
+            prevElement.addClass("active");
+
+    };
+
+
+
+/*var slider = new nav(document.querySelectorAll('#img_pos')); 
+//var images = document.querySelectorAll('.img_pos'); 
+console.log(nav);     
+function nav(arr) {
+    var current = 0, len = arr.length - 1;
+    this.next = function () {
+        current =  current < len ? current + 1 : 0;
+        return arr[current];
+
+    };
+    this.prev = function () {
+        current = current > 0 ? current - 1 : len;
+        return arr[ current];
+    };
+}
+
+
+
+document.getElementById("next").onclick = function() {
+  var len = slider.length;
+  while (len--) slider[len].className = 'img_pos';
+  slider.next().className += ' active';
 	
+/*	 var len = slider.length;
+  	while (len--) slider[len].className = 'img_pos';
+slider.next().className += ' active';
+*/
+
+/*};
+
+prev.onclick = function () {
+    slider.prev().className += ' active';
+     var len = slider.length;
+  		while (len--) slider[len].className = 'img_pos';
+};
+
+p_button.onclick = function(){
+	ell = document.getElementsByClassName("p_demo1");
+		//alert(ell);
+		ell.className ="";
+			ell.className = "p_demo2";
+};
+*/
 
 
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------
 //console.log(slides)
 
 
 
-	var images = document.querySelectorAll('.img_pos'),
+/*	var images = document.querySelectorAll('.img_pos');
   slider = next(images);
   s_prev = prev(images);
-  console.log(images)
-  i_pos = document.getElementsByClassName("active");
-  console.log(i_pos)
- 
+  console.log(images);
+
+  function imgPos() {
+  active = document.querySelectorAll(".active");
+  console.log(active)
+}
+imgPos();
+
+ // i_pos = document.getElementsByClassName(".active").indexOf(active);
+  
+  //i_pos = images.indexOf(".active");
+// console.log(i_pos);
 
 //Position of image
 
-function position() {
-    var fruits = ["Banana", "Orange", "Apple", "Mango"];
-    var a = fruits.indexOf("Apple");
+/*function position() {
+    var fruits = document.querySelectorAll('.img_pos');
+    var a = fruits.indexOf(".active");
     document.getElementById("demo").innerHTML = a;
 }
 
 position()
+
 
 
 
@@ -56,24 +138,25 @@ document.getElementById("next").onclick = function() {
   var len = images.length;
   while (len--) images[len].className = 'img_pos';
   slider().className += ' active';
+  imgPos();
 }
 
 //      PREV
 
 function prev(arr) {
   var max = 0;
-      i = arr.length - 1;
+      i =  arr.length -1;
   return function() {
     i = i < max ? i + 1 : i;
-    return arr[i--];
- 
+ 	return arr[i--];	
    };
 };
 
 document.getElementById("prev").onclick = function() {
-  var len = images.length;
+  var len = images.length ;
   while (len--) images[len].className = 'img_pos';
   s_prev().className += ' active';
+  imgPos()
 
 };
 /*		function imagePosition(){
@@ -82,13 +165,14 @@ document.getElementById("prev").onclick = function() {
 		}
 
 imagePosition()
-*/
-};
-
-
-/*
-    var img =  images.indexOf(document.getElementsByClassName("active"));
-
-
 
 */
+
+
+
+
+
+
+    
+
+
